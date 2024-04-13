cbed8419232fff5b4585e3cb3ec134340e76d1b6
Abhijeet1710 <abhijeetkhamkar30@gmail.com>
2024-04-13 15:15:52 +0530

    Created user-details api

--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -3,8 +3,28 @@ const { UserCollection } = require("../../db/db.js");
 
 const router = Router()
 
-router.get("/profile", async (req, res) => {
-    res.json({msg: "This is your Profile Data"})
+router.get("/user-details/:userName", async (req, res) => {
+    const userName = req.params.userName
+    
+    try {
+        const userDetails = await UserCollection.findOne({
+            $or: [{email: userName}, {userName: userName}]
+        })
+        if(userDetails) {
+            return res.json({
+                message: "Success",
+                userDetails
+            })
+        } else {
+            return res.status(404).json({
+                message: `User ${userName} not found`
+            })
+        }
+        
+    }catch(err) {
+    }
+
+    res.status(500).json({msg: "Internal Server Error"})
 })
 
 
