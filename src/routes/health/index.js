d4c9cd4dd94d8472ff70b6a3e586c426a6a2f9e0
Abhijeet1710 <abhijeetkhamkar30@gmail.com>
2024-04-13 22:59:57 +0530

    Icon added

--- src/routes/health/index.js
+++ src/routes/health/index.js
@@ -5,7 +5,7 @@ const router = Router()
 router.get("/healthCheck", (req, res) => {
     res.status(200).json({
         statusCode: 200,
-        message: "Hey Aditya ! Server is Up And healthy.."
+        message: "Hey Aditya ! Server is Up And healthy... 🚀"
     })
 })
 
